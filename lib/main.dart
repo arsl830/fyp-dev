4f19752f78183b4d729b05f363b9ee5fcdf447cd
Hamza Khan <74649522+HamzaaaKhan@users.noreply.github.com>
2021-12-28 19:21:53 +0500

    sign_up module

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,227 @@
+// main.dart
 import 'package:flutter/material.dart';
-import 'package:fyp_dev/login.dart';
+//import 'package:getwidget/getwidget.dart';
+
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
-    routes: {'login': (context) => MyLogin()},
-  )
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Define a key to access the form
+  final _formKey = GlobalKey<FormState>();
+
+  String _userEmail = '';
+  String _userName = '';
+  String _password = '';
+  String _confirmPassword = '';
+
+  // This function is triggered when the user press the "Sign Up" button
+  void _trySubmitForm() {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      print('Everything looks good!');
+      print(_userEmail);
+      print(_userName);
+      print(_password);
+      print(_confirmPassword);
+
+      /* 
+      for processing data in database or api calling 
+      */
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        centerTitle: true,
+      ),
+      //  Container(
+      //               alignment: Alignment.center,
+      //               padding: EdgeInsets.all(10),
+      //               child: Text(
+      //                 'Sign in',
+      //                 style: TextStyle(fontSize: 20),
+      //               )),
+
+      body: Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Center(
+          child: Card(
+            margin: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+            // child: Padding(
+            //   // padding: const EdgeInsets.all(10),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    /////////////////////////////////////////
+                    /// Eamil
+                    Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Create Account',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 40),
+                        )),
+                    Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Create Account',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 40),
+                        )),
+                    Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'Or Sign up with Email',
+                          style: TextStyle(fontSize: 20),
+                        )),
+
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0)),
+                        labelText: 'Email',
+                      ),
+                      validator: (value) {
+                        if (value!.trim().isEmpty) {
+                          return 'Please enter your email address';
+                        }
+                        // Check if the entered email has the right format
+                        if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        // Return null if the entered email is valid
+                        return null;
+                      },
+                      onChanged: (value) => _userEmail = value,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                    /////////////////////////////////////////
+                    /// username
+
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0)),
+                          labelText: 'Username'),
+                      validator: (value) {
+                        if (value!.trim().isEmpty) {
+                          return 'This field is required';
+                        }
+                        if (value.trim().length < 4) {
+                          return 'Username must be at least 4 characters in length';
+                        }
+                        // Return null if the entered username is valid
+                        return null;
+                      },
+                      onChanged: (value) => _userName = value,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                    ////////////////////////////////////
+                    /// Password
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0)),
+                          labelText: 'Password'),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.trim().isEmpty) {
+                          return 'This field is required';
+                        }
+                        if (value.trim().length < 8) {
+                          return 'Password must be at least 8 characters in length';
+                        }
+                        // Return null if the entered password is valid
+                        return null;
+                      },
+                      onChanged: (value) => _password = value,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    //////////////////////////////////
+                    /// Confirm Password
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0)),
+                          labelText: 'Confirm Password'),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'This field is required';
+                        }
+
+                        if (value != _password) {
+                          return 'Confimation password does not match the entered password';
+                        }
+
+                        return null;
+                      },
+                      onChanged: (value) => _confirmPassword = value,
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                        width: 300.0,
+                        height: 50.0,
+                        child: OutlinedButton(
+                            child: Text('Sign Up'),
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.white,
+                              backgroundColor: Colors.teal,
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                            ),
+                            onPressed: _trySubmitForm)),
+                  ],
+                )),
+          ),
+        ),
+      ),
+    );
+  }
 }
